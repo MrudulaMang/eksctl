ef1bf46d0a44956a9d6578a06c5bf40c58ecc7ed
mrudu <mrudu@example.com>
2026-04-06 13:32:13 +0530

    ec2

---
+++ ec2/disk.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+growpart /dev/nvme0n1 4 #grow full size
+pvresize /dev/nvme0n1p4 #i wrote coz lvextend may fail
+lvextend -r -L +30G /dev/mapper/RootVG-varVol
+xfs_growfs /var
+# xfx_growfs /home ----no need to use this command when using -r in the above
+
+#installing terraform inbastian to execute terra cmds to run ansible playbook in mongo thru terra to install mongodb database
+#yum install -y yum-utils 
+#yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+#yum -y install terraform
+
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ec2-user
+# do docker login
+
+
+# ################  EKSCTL session 55
+
+# # for ARM systems, set ARCH to: `arm64`, `armv6` or `armv7`
+# ARCH=amd64
+# PLATFORM=$(uname -s)_$ARCH
+
+# curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+
+# # (Optional below command) Verify checksum 
+# curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+
+
+# tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+# sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
+# eksctl version
+
+
+# curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.35.2/2026-02-27/bin/linux/amd64/kubectl
+# chmod +x ./kubectl
+# mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
+
+#aws configure
+
+# create CLUSTER
+# eksctl create cluster --config-file = eks.yaml
+
